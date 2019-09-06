9ee05b9ce17034883a06772037bd2168329e4ebe
drbarq <J.Tustin@gmail.com>
2019-09-06 16:40:19 -0600

    got delete working

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,6 +4,8 @@ import CreateDataContext from './createDataContext'
 // example of using createContext reusable component
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload )
         case 'add_blogpost':
             return [
                 ...state, 
@@ -23,9 +25,15 @@ const addBlogPost = dispatch => {
     }
 }
 
+const deleteBlogPost = dispatch => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id})
+    }
+}
+
 export const { Context, Provider } = CreateDataContext( 
     blogReducer, 
-    { addBlogPost: addBlogPost },
+    { addBlogPost: addBlogPost, deleteBlogPost: deleteBlogPost },
     []
 )
 
