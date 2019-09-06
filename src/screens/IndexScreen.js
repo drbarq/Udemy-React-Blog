b89ca75af0d758e6ac8d4bc7c6461cec7259b11b
drbarq <J.Tustin@gmail.com>
2019-09-06 16:46:59 -0600

    adding show navigator

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons'
 
-const IndexScreen = () => {
+const IndexScreen = ( props ) => {
     const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
     return (
@@ -17,17 +17,19 @@ const IndexScreen = () => {
                 keyExtractor={ (blogPost) => blogPost.title}
                 renderItem={({item}) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>
-                                {item.title} - {item.id} 
-                            </Text>
-                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                                <Feather 
-                                    style={styles.icon} 
-                                    name="trash" 
-                                />
-                            </TouchableOpacity>
-                        </View>
+                        <TouchableOpacity onPress={() => props.navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>
+                                    {item.title} - {item.id} 
+                                </Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather 
+                                        style={styles.icon} 
+                                        name="trash" 
+                                    />
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     )
                 }}
             />
