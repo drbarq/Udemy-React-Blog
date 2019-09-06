95473cd14594e0a4d20be9d27a10ddbf268e3897
drbarq <J.Tustin@gmail.com>
2019-09-06 15:28:10 -0600

    refactoring context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,7 @@
-import React, { useReducer } from 'react'
+import createContext from './createDataContext'
+import CreateDataContext from './createDataContext'
 
-const BlogContext = React.createContext()
-
+// example of using createContext reusable component
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
@@ -11,21 +11,46 @@ const blogReducer = (state, action) => {
     }
 }
 
-export const BlogProvider = ( {children} ) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-    const addBlogPost = () => {
-        dispatch({ type: 'add_blogpost' })
-    }
-
-    return (
-        <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
-            {children}
-        </BlogContext.Provider>
-    ) 
+const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost' })
 }
 
-export default BlogContext
+export const { Context, Provider } = CreateDataContext( 
+    blogReducer, 
+    { addBlogPost: addBlogPost },
+    []
+)
+
+
+// example of using specific Context and Provider 
+
+// import React, { useReducer } from 'react'
+// const BlogContext = React.createContext()
+
+// const blogReducer = (state, action) => {
+//     switch (action.type) {
+//         case 'add_blogpost':
+//             return [...state, { title: `Blog Post # ${state.length + 1}`}]
+//         default:
+//             return state
+//     }
+// }
+
+// export const BlogProvider = ( {children} ) => {
+//     const [blogPosts, dispatch] = useReducer(blogReducer, [])
+
+//     const addBlogPost = () => {
+//         dispatch({ type: 'add_blogpost' })
+//     }
+
+//     return (
+//         <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+//             {children}
+//         </BlogContext.Provider>
+//     ) 
+// }
+
+// export default BlogContext
 
 
 
---
+++ src/context/createDataContext.js
@@ -0,0 +1,19 @@
+import React, { useReducer } from 'react'
+
+export default (reducer, actions, initalState) => {
+    const Context = React.createContext()
+
+    const Provider = ({ children }) => {
+        const [state, dispatch] = useReducer(reducer, initalState)
+
+        return (
+            <Context.Provider
+                value={{ state: state }}
+            >
+                { children }
+            </Context.Provider>
+        )
+    }
+
+    return { Context, Provider }
+}
