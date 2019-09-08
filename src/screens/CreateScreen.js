20029eb6e0941305b30a2df2a91249af95d357b7
drbarq <J.Tustin@gmail.com>
2019-09-08 12:07:04 -0600

    destructuring new post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,54 +1,16 @@
 import React, { useContext, useState } from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 import {Context as Context} from '../context/BlogContext'  
+import BlogPostForm from '../components/BlogPostForm'
     // example of renaming context
 
 const CreateScreen = ( props ) => {
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
     const { addBlogPost } = useContext(Context)
-        // destructure off of context
 
-    return (
-        <View>
-            <Text style={styles.label}>Enter Title</Text>
-            <TextInput 
-                style={styles.input} 
-                value={title} 
-                onChangeText={(text) => setTitle(text)} 
-            />
-            <Text style={styles.label}>Enter Content</Text>
-            <TextInput 
-                style={styles.input} 
-                value={content} 
-                onChangeText={(content) => setContent(content)} 
-            />
-            <Button 
-                title="Add Blog Post"
-                onPress={() => addBlogPost(title, content, () => {
-                    props.navigation.navigate('Index')
-                })}
-            >
-            </Button>
-        </View>
-    )
+    return <BlogPostForm />
 }
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 18, 
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5
-    }
-})
+const styles = StyleSheet.create({})
 
 export default CreateScreen
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,22 +1,14 @@
 import React, { useContext, useState } from 'react'
 import { Text, View, StyleSheet, TextInput } from 'react-native'
 import { Context } from '../context/BlogContext'
+import BlogPostForm from '../components/BlogPostForm'
 
 const EditScreen = (props) => {
     const { state } = useContext(Context)
     const blogPost = state.find((blogPost) => blogPost.id === props.navigation.state.params.id)
 
-    const [title, setTitle] = useState(blogPost.title)
-    const [content, setContent] = useState(blogPost.content)
-
     return (
-        <View>
-            <Text>Edit Title:</Text>
-            <TextInput 
-                value={title}
-                onChangeText={(newTitle) => setTitle(newTitle)}>
-            </TextInput>
-        </View>
+        <BlogPostForm></BlogPostForm>
     )
 }
 
